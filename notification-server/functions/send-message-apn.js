63da8956e93f82d436df999862f0517c76522d8c
Santiago Apaza <sapazad@gmail.com>
2017-01-12 12:25:51 -0500

    server notification

---
+++ notification-server/functions/send-message-apn.js
@@ -0,0 +1,61 @@
+var apn  = require("apn");
+var constants = require('../constants/constants.json');
+
+var apnError = function(err){
+    console.log("APN Error:", err);
+}
+
+var options = {
+    "cert": "cert.pem",
+    "key":  "key.pem",
+    "passphrase": null,
+    "gateway": "gateway.sandbox.push.apple.com",
+    "port": 2195,
+    "enhanced": true,
+    "cacheLength": 5
+  };
+options.errorCallback = apnError;
+
+var feedBackOptions = {
+    "batchFeedback": true,
+    "interval": 300
+};
+
+var apnConnection, feedback;
+
+module.exports = {
+    init : function(){
+        apnConnection = new apn.Connection(options);
+
+        feedback = new apn.Feedback(feedBackOptions);
+        feedback.on("feedback", function(devices) {
+            devices.forEach(function(item) {
+                //TODO Do something with item.device and item.time;
+            });
+        });
+    },
+
+    send : function (params){
+        var myDevice, note;
+        
+        myDevice = new apn.Device(params.token);
+        note = new apn.Notification();
+
+        note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+        note.badge = 1;
+        note.sound = "ping.aiff";
+        note.alert = params.message;
+        note.payload = {'messageFrom': params.from};
+
+        if(apnConnection) {
+            apnConnection.pushNotification(note, myDevice);
+        }
+    }
+}
+
+/*usage
+pushNotifier = require("./pushNotifier");
+pushNotifier.init();
+//use valid device token to get it working 
+pushNotifier.process({token:'', message:'Test message', from: 'sender'});
+*/
