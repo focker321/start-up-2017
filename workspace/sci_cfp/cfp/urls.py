88349825b3f849bb94f83c89f9bef093ad8f6004
jzelar <jzelar@gmail.com>
2017-01-28 17:38:14 -0500

    User Accounts

--- workspace/sci_cfp/cfp/urls.py
+++ workspace/sci_cfp/cfp/urls.py
@@ -4,4 +4,7 @@ from cfp import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'login', views.login_user, name='login'),
+    url(r'signup', views.signup_user, name='signup'),
+    url(r'logout', views.logout_user, name='logout'),
 ]
--- workspace/sci_cfp/cfp/views.py
+++ workspace/sci_cfp/cfp/views.py
@@ -1,7 +1,65 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.db import IntegrityError
 
 
 def index(request):
     return render(request, 'cfp/index.html', dict())
+
+
+@csrf_protect
+def login_user(request):
+    if request.method == 'GET':
+        return render(request, 'cfp/login.html', dict())
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        next = request.POST.get('next', '/')
+
+        response_dict = dict()
+        user = authenticate(username=email, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                #request.session.set_expiry(30)
+                return HttpResponseRedirect(next)
+            else:
+                response_dict['login_error'] = "Account is not active!"
+        else:
+            response_dict['login_error'] = "Incorrect email or password!"
+
+        response_dict['email'] = email
+        return render(request, 'cfp/login.html', response_dict)
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def signup_user(request):
+    if request.method == 'GET':
+        return render(request, "cfp/signup.html", dict())
+    elif request.method == 'POST':
+        first_name = request.POST.get('first_name', '')
+        # last_name = request.POST.get('last_name', '')
+        email = request.POST.get('email', '').lower()
+        password = request.POST.get('password', '')
+        try:
+            user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name)
+            user.is_active = True
+            user.save()
+        except IntegrityError:
+            error = "Email address already registered."
+            return render(request, "cfp/signup.html", dict(signup_error=error))
+
+        return render(request, "cfp/confirmation.html", dict(type='signup'))
+    else:
+        return render(request, 'cfp/login.html', dict())
