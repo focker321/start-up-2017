62131450455f88e3633733ef45dd5cc1894c6197
mailaucq <lvcruzq@gmail.com>
2017-01-29 23:09:21 -0500

    set events in database - incomplete

--- workspace/sci_cfp/cfp/models.py
+++ workspace/sci_cfp/cfp/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    acronym = models.CharField(max_length=500,)
+    title_event = models.CharField(max_length=1000,)
+    submission_deadline = models.DateField()
+    event_url = models.CharField(max_length=1000,)
+    end_date = models.DateField()
+    camera_ready = models.DateField()
+    type_event = models.CharField(max_length=255,)
+    notification_date = models.DateField()
+    location = models.CharField(max_length=500,)
+    start_date = models.DateField()
+    categories = models.CharField(max_length=1000,)
+
+    def __str__(self):
+        return self.acronym
--- workspace/sci_cfp/crawler/crawler.py
+++ workspace/sci_cfp/crawler/crawler.py
@@ -2,7 +2,11 @@ __author__ = 'laura'
 from bs4 import BeautifulSoup
 import re
 import urllib2
-import json
+# import json
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import sci_cfp.settings
+from cfp.models import Event
 
 
 class Crawler():
@@ -29,7 +33,8 @@ class Crawler():
                 page = urllib2.urlopen(event_url)
                 soup2 = BeautifulSoup(page, "lxml")
                 event_properties = soup2.findAll('span', typeof=re.compile('v:Event'))
-                event = dict()
+                # event = dict()
+                event = Event()
                 for event_property in event_properties:
                     name_property = self.get_prop_span(event_property.find('span', property=re.compile('v:summary')), "content")
                     start_property = self.get_prop_span(event_property.find('span', property=re.compile('v:startDate')), "content")
@@ -38,32 +43,43 @@ class Crawler():
                     locality_property = self.get_prop_span(event_property.find('span', property=re.compile('v:locality')), "content")
                     description_property = self.get_prop_span(event_property.find('span', property=re.compile('v:description')), "text")
                     if name_property == 'Submission Deadline':
-                        event["submission_deadline"] = start_property
+                        # event["submission_deadline"] = start_property
+                        event.submission_deadline = start_property
                     if name_property == 'Notification Due':
-                        event["notification_date"] = start_property
+                        # event["notification_date"] = start_property
+                        event.notification_date = start_property
                     if name_property == 'Final Version Due':
-                        event["camera_ready"] = start_property
+                        # event["camera_ready"] = start_property
+                        event.camera_ready = start_property
                     if len(description_property) > 0:
-                        event["acronym"] = name_property
-                        event["title_event"] = description_property
-                        event["start_date"] = start_property
-                        event["end_date"] = end_property
+                        # event["acronym"] = name_property
+                        event.acronym = name_property
+                        # event["title_event"] = description_property
+                        event.title_event = description_property
+                        # event["start_date"] = start_property
+                        event.start_date = start_property
+                        # event["end_date"] = end_property
+                        event.end_date = end_property
                     if len(locality_property) > 0:
-                        event["location"] = locality_property
+                        # event["location"] = locality_property
+                        event.location = locality_property
                     if len(type_property) > 0:
-                        event["type_event"] = type_property
-                    break
+                        #event["type_event"] = type_property
+                        event.type_event = type_property
                 categories = soup2.findAll("a", href=re.compile('conference'))
                 categories_name = []
                 for cat in categories:
                     cat_name = self.get_prop_span(cat, "text").encode("utf-8")
                     categories_name.append(cat_name)
-                event["categories"] = ", ".join([c for c in categories_name])
-                event["event_url"] = self.get_prop_span(soup2.find("a", target="_newtab"), "href")
-                events.append(event)
+                # event["categories"] = ", ".join([c for c in categories_name])
+                event.categories = ", ".join([c for c in categories_name])
+                # event["event_url"] = self.get_prop_span(soup2.find("a", target="_newtab"), "href")
+                event.event_url = self.get_prop_span(soup2.find("a", target="_newtab"), "href")
+                # events.append(event)
+                event.save()
         # print(events)
-        with open('data.json', 'w') as fp:
-            json.dump(events, fp, indent=4)
+        # with open('data.json', 'w') as fp:
+        #    json.dump(events, fp, indent=4)
 if __name__ == '__main__':
     crawler = Crawler()
     crawler.crawler()
