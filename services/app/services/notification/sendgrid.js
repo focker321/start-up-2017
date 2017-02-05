af6049a323cb65f93bfb7fe2494ffc45a42cadd3
Santiago Apaza <sapazad@gmail.com>
2017-02-05 10:16:26 -0500

    added templating sendgrid mailing

--- services/app/services/notification/sendgrid.js
+++ services/app/services/notification/sendgrid.js
@@ -1,5 +1,10 @@
 var helper = require('sendgrid').mail;
+
+
+
 var sg = require('sendgrid')("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");
+const MY_TEMPLATE_ID = 'd2111ea7-0cea-4e1c-a9ad-345cc0937958'
+
 
 function Mail_Sender(from, to) {
   this.from = from;
@@ -15,11 +20,24 @@ Mail_Sender.prototype.createMail = function () {
   var from_email = new helper.Email(this.from);
   var to_email = new helper.Email(this.to);
   var subject = this.subject;
-  var content = new helper.Content('text/plain', this.content);
+  // var content = new helper.Content('text/plain', this.content);
+  var content = new helper.Content(
+    'text/html', 'I\'m replacing the <strong>body tag</strong>');
 
   var mail = new helper.Mail(from_email, subject, to_email, content);
 
+  // mail.personalizations[0].addSubstitution(
+  //   new helper.Substitution('-name-', 'Example User'));
+  // mail.personalizations[0].addSubstitution(
+  //   new helper.Substitution('-city-', 'Denver'));
+  mail.setTemplateId(MY_TEMPLATE_ID);
+
+
+
+
   return mail;
+
+
 };
 
 Mail_Sender.prototype.sendMail = function (mail) {
--- services/server.js
+++ services/server.js
@@ -13,6 +13,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 var port = process.env.PORT || 8080;        // set our port
 
 
