100e0d74181cc4c0e8b5bc8e2e5dd22bd4d2951f
Santiago Apaza <sapazad@gmail.com>
2017-02-12 13:20:22 -0500

    adicionado enter al inputbox

--- workspace/sci_cfp/cfp/static/cfp/js/search.js
+++ workspace/sci_cfp/cfp/static/cfp/js/search.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
-    $('#sci-cfp-search-btn').click(function() {
+$(document).ready(function () {
+    $('#sci-cfp-search-btn').click(function () {
         window.location.href = '/cfp/search?q=' + $('#sci-cfp-search-in').val();
     });
+
+    $("#sci-cfp-search-in").bind("keypress", {}, keypressInBox);
+
+    function keypressInBox(e) {
+
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code == 13) { //Enter keycode                        
+            e.preventDefault();
+            window.location = "/buscador";
+        }
+    }
 });
