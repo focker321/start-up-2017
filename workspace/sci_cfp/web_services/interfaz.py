0645ccb4b973a8c101905596ab8964cacb1dee1c
Santiago Apaza <nuss@santiagos-mbp.pe.ibm.com>
2017-02-28 14:41:27 -0500

    sendgrid since python

--- workspace/sci_cfp/web_services/interfaz.py
+++ workspace/sci_cfp/web_services/interfaz.py
@@ -3,6 +3,16 @@ __author__ = 'NuSs'
 import requests
 import json
 
+import sendgrid
+import os
+from sendgrid.helpers.mail import Email, Content, Substitution, Mail
+try:
+    # Python 3
+    import urllib.request as urllib
+except ImportError:
+    # Python 2
+    import urllib2 as urllib
+
 class Interfaz():
     def __init__(self):
         # Local settings
@@ -26,14 +36,24 @@ class Interfaz():
         print(req.status_code, req.reason)
 
     def send_mail_notification(self, to, message):
-
-        payload = {"from": "sapazad@gmail.com",
-                   "to": to,
-                   "message": message}
-
-        req = requests.post("http://localhost:8080/api/send_mail", data=json.dumps(payload))
-
-        print(req.status_code, req.reason)
+        sg = sendgrid.SendGridAPIClient(apikey="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")
+        my_template_id = 'd2111ea7-0cea-4e1c-a9ad-345cc0937958'
+        from_email = Email("sapazad@gmail.com")
+        subject = "Hello World from the SendGrid Python Library!"
+        to_email = Email(to)
+        content = Content("text/html", "I'm replacing the <strong>body tag</strong>")
+        mail = Mail(from_email, subject, to_email, content)
+        # mail.personalizations[0].add_substitution(Substitution("-name-", "Example User"))
+        # mail.personalizations[0].add_substitution(Substitution("-city-", "Denver"))
+        mail.set_template_id(my_template_id)
+        try:
+            response = sg.client.mail.send.post(request_body=mail.get())
+        except urllib.HTTPError as e:
+            print e.read()
+            exit()
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
 
 if __name__ == '__main__':
     interfaz = Interfaz()
